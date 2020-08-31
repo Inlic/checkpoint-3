344dfab2b8695d3c4700eb7e19ba7fc222f77063
Ben W <bwiedeback@gmail.com>
2020-08-30 20:18:33 -0600

    updated messaging

--- app/store.js
+++ app/store.js
@@ -4,7 +4,7 @@ import List from "./Models/List.js";
 let _state = {
   /**@type {List[]} */
   lists: [
-    // TODO test list for template
+    // NOTE test list for template
     new List({title: "Groceries", items: [{ item:"Bread", checked: false},{item:"Eggs",checked: false},{item:"Milk",checked:false}], color:"#ff7034",}),
     new List({title: "Chores", items: [{ item:"Take out trash", checked: false},{item:"Clean bathrooms",checked: false},{item:"Do laundry",checked:false}], color:"#08bf11",}),
     new List({title: "Vacation Plan", items: [{ item:"Book Flight", checked: false},{item:"Hire Guide",checked: false},{item:"Set up restaurant tour",checked:false}], color:"#b134eb",})
